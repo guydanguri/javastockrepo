648af99f3a5affee277c0c645f298f009891d80a
יוסי <@192.168.0.103>
2015-04-05 12:02:14 +0300

    ex02

--- src/com/mta/javacourse/Guy_NasdaqServlet.java
+++ src/com/mta/javacourse/Guy_NasdaqServlet.java
@@ -5,7 +5,19 @@ import javax.servlet.http.*;
 @SuppressWarnings("serial")
 public class Guy_NasdaqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.setContentType("text/html");
+	
+		int num1,num2,num3;
+		num1=3;
+		num2=4;
+		num3=7;
+		
+		int result= (num1+num2)*num3;
+		
+		String resultstr= new String(("<h1>Result of ("+num1+"+"+num2+")"+"*"+num3+"="+result+"</h1>"));
+		
+		resp.getWriter().println(resultstr);
+		
 	}
 }
+
